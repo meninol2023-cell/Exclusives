73d3a6088c805aae5afa7edd3c8ccc7bbb210b82
meninol2023-cell <meninol2023@gmail.com>
2026-04-18 14:56:44 -0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,13 @@
+require("./server");
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`🤖 Online como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
